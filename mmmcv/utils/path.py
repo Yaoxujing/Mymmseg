62fe8b4cfdb51dd280fdaa2f7e9006bc593aeeb2
Yaoxujing <387951031@qq.com>
2023-02-10 22:09:29 +0800

    config

---
+++ mmmcv/utils/path.py
@@ -0,0 +1,9 @@
+import os
+import os.path as osp
+from pathlib import Path
+
+from .misc import is_str
+
+def check_file_exist(filename, msg_tmpl='file "{}" does not exist'):
+    if not osp.isfile(filename):
+        raise FileNotFoundError(msg_tmpl.format(filename))
